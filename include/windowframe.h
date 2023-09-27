854a9ac6e3a92ebfcfcacc9f4bb54ed184e1c2ca
go214621 <go214621@misdmail.net>
2023-09-26 20:04:11 -0500

    Day 6 - Nearly done
    Just needs images

--- include/windowframe.h
+++ include/windowframe.h
@@ -10,13 +10,13 @@ public:
 
 	// functions
 	WindowFrame(const wxString& title);
-	void makeBold(wxStaticText*);
+	static void makeBold(wxStaticText*);
 };
 
 enum PageID {
 	PageID_CellOverview,
 	PageID_Organelles,
-	PageID_OrganellesContinued,
+	PageID_OrganellesCont,
 	PageID_CellFunctions, // will also contain misc functions in more detail
 	PageID_About
 };
@@ -71,15 +71,51 @@ WindowFrame::WindowFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, tit
 	makeBold(endomembrane);
 	wxStaticText* endomembraneDesc = new wxStaticText(notebook->GetPage(PageID_Organelles), wxID_ANY, "- Nuclear Envelope\n- Endoplasmic Reticulum\n- Golgi Complex\n- Lysosomes\n- Vesicles/Vacuoles\n- Plasma Membranes", wxPoint(10, 90));
 
+	wxStaticText* compart = new wxStaticText(notebook->GetPage(PageID_Organelles), wxID_ANY, "Compartmentalisation", wxPoint(10, 200));
+	makeBold(compart);
+	wxStaticText* compartDesc = new wxStaticText(notebook->GetPage(PageID_Organelles), wxID_ANY, "allows for different metabolic reactions to occur in different places, and\n prevents reactions from interfering with each other.", wxPoint(10, 215));
+
 	wxStaticText* energy = new wxStaticText(notebook->GetPage(PageID_Organelles), wxID_ANY, "Energy", wxPoint(250, 70));
 	makeBold(energy);
 	wxStaticText* energyDesc = new wxStaticText(notebook->GetPage(PageID_Organelles), wxID_ANY, "- Mitochondria (\"the powerhouse of the cell\")\n- Chloroplasts", wxPoint(250, 90));
 
 	wxStaticBitmap* uniqueCellComponents = new wxStaticBitmap(notebook->GetPage(PageID_Organelles), wxID_ANY, wxBitmap("res/unique_cell_components.png", wxBITMAP_TYPE_PNG), wxPoint(10, 300));
 
-	wxStaticText* nucleus = new wxStaticText(notebook->GetPage(PageID_Organelles), wxID_ANY, "Nucleus - stores the nucleolus.\nNucleolus - stores genetic information (chromosomes). DNA\nCANNOT leave here. rRNA combined with\nproteins form subunits of\nribosomes, which exit through\nnuclear pores.", wxPoint(550, 10));
-	wxStaticBitmap* nucleusImage = new wxStaticBitmap(notebook->GetPage(PageID_Organelles), wxID_ANY, wxBitmap("res/organelle_nucleus.png", wxBITMAP_TYPE_PNG), wxPoint(550, 130));
-	wxStaticBitmap* nuclearEnvelope = new wxStaticBitmap(notebook->GetPage(PageID_Organelles), wxID_ANY, wxBitmap("res/organelle_nuclear_envelope.png", wxBITMAP_TYPE_PNG), wxPoint(550, 400));
+	wxStaticText* nucleus = new wxStaticText(notebook->GetPage(PageID_Organelles), wxID_ANY, "Nucleus - stores the nucleolus.\nNucleolus - stores genetic information (chromosomes). DNA\nCANNOT leave here. rRNA combined with proteins form\nsubunits of ribosomes, which exit through nuclear pores.", wxPoint(550, 10));
+	wxStaticBitmap* nucleusImage = new wxStaticBitmap(notebook->GetPage(PageID_Organelles), wxID_ANY, wxBitmap("res/organelle_nucleus.png", wxBITMAP_TYPE_PNG), wxPoint(550, 110));
+	wxStaticBitmap* nuclearEnvelope = new wxStaticBitmap(notebook->GetPage(PageID_Organelles), wxID_ANY, wxBitmap("res/organelle_nuclear_envelope.png", wxBITMAP_TYPE_PNG), wxPoint(550, 390));
 
-	
+	// organelles, continued
+
+
+
+	wxStaticText* ribosomes = new wxStaticText(notebook->GetPage(PageID_OrganellesCont), wxID_ANY, "Ribosomes", wxPoint(10, 10));
+	makeBold(ribosomes);
+	wxStaticText* ribosomesDesc = new wxStaticText(notebook->GetPage(PageID_OrganellesCont), wxID_ANY, "Comprised of ribosomal RNA and proteins.\nIts function is to synthesise proteins.\nCan be found in one of two locations:\nCytosol and bound either the ER or nuclear envelope.\nProteins made there go outside the cell.", wxPoint(10, 25));
+
+	/* add image - ribosomes */
+
+	wxStaticText* er = new wxStaticText(notebook->GetPage(PageID_OrganellesCont), wxID_ANY, "Endoplasmic Reticulum", wxPoint(10, 10));
+	makeBold(er);
+	wxStaticText* erDesc = new wxStaticText(notebook->GetPage(PageID_OrganellesCont), wxID_ANY, "- Synthesises membranes\n- Compartmentalises the cell to keep proteins\nformed in the rough ER\nThere are two types:\n- ROUGH\n- SMOOTH\nThe rough ER contains ribosomes on its membrane\nThe smooth ER syntehsises lipids, metabolises\ncarbs, and detoxifies the cell.", wxPoint(10, 100));
+
+	/* add image - rough and smooth ER */
+
+	wxStaticText* gc = new wxStaticText(notebook->GetPage(PageID_OrganellesCont), wxID_ANY, "Golgi Complex", wxPoint(300, 10));
+	makeBold(gc);
+	wxStaticText* gcDesc = new wxStaticText(notebook->GetPage(PageID_OrganellesCont), wxID_ANY, "Contains flattened membranous sacs called Cisternae\n- This separates the sacs from the cytosol\n- Each cisternae is not connected\nHas differentiality\nCis face - receives vesicles from ER\nTrans face - Sends vesicles out into cytosol, or into the\nplasma membrane for secretion\nThe golgi complex modifies and sorts materials,\nadds molecular tags, and packages materials to exit the cell.", wxPoint(300, 30));
+
+	/* add image - golgi complex */
+
+	wxStaticText* lysosome = new wxStaticText(notebook->GetPage(PageID_OrganellesCont), wxID_ANY, "Lysosomes", wxPoint(600, 10));
+	makeBold(lysosome);
+	wxStaticText* lysosomeDesc = new wxStaticText(notebook->GetPage(PageID_OrganellesCont), wxID_ANY, "Membranous sac with hydrolytic enzymes\nHydro - water\nlytic - break\nThe lysosome hydrolyses macromolecules in the cell\nSimilar to to lysosomes, Peroxisomes\ncatalyse reactions that produce peroxide (H2O2).\nAutophagy allows the cell to renew itself.", wxPoint(600, 30));
+
+	/* add image - lysosomes */
+
+	wxStaticText* vacuole = new wxStaticText(notebook->GetPage(PageID_OrganellesCont), wxID_ANY, "Vacuole", wxPoint(900, 10));
+	makeBold(vacuole);
+	wxStaticText* vacuoleDesc = new wxStaticText(notebook->GetPage(PageID_OrganellesCont), wxID_ANY, "Large vesicles that stem from the ER and Golgi. There are multiple types:\n- Food Vacuole (form in Phagocytosis)\n- Contractile Vacuole\n- Central Vacuole (found in plants)", wxPoint(900, 30));
+
+	/* add image - vacuoles */
 }	
